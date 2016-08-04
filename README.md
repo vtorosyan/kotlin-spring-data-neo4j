3cbe20289dbb073deaf4d0232ef70b84d0c24067
Vardan Torosyan <vardan.torosyan@friendsurance.de>
2016-08-04 17:52:32 +0200

    Movie Application Example using Kotlin with Spring Data, Spring Web and Neo4J

---
+++ README.md
@@ -0,0 +1,2 @@
+# kotlin-spring-data-neo4j
+Kotlin with Spring Data and Spring Web - Movies Application Example
--- src/test/kotlin/com/examples/kotlin/TestConfig.kt
+++ src/test/kotlin/com/examples/kotlin/TestConfig.kt
@@ -2,7 +2,7 @@ package com.examples.kotlin
 
 import com.examples.kotlin.service.MovieService
 import com.examples.kotlin.service.PersonService
-import org.mockito.Mockito
+import org.mockito.Mockito.mock
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Profile
@@ -13,13 +13,13 @@ open class TestConfig {
     @Bean
     @Profile("test")
     open fun personService(): PersonService {
-        return Mockito.mock(PersonService::class.java)
+        return mock(PersonService::class.java)
     }
 
     @Bean
     @Profile("test")
     open fun movieService(): MovieService {
-        return Mockito.mock(MovieService::class.java)
+        return mock(MovieService::class.java)
     }
 
 }
